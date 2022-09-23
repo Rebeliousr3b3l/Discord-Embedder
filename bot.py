10fcb9ea390434a2cf752dcd62b17bef7df5a9b7
rebelliousr3b3l <jdfortner00@gmail.com>
2022-09-23 14:37:04 -0500

    Adds "vx" to the beginning of "twitter".

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,8 +12,12 @@ class MyClient(discord.Client):
     
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        if "twitter" in message.content:
+        if "twitter" in message.content and not "vxtwitter" in message.content:
             print("I see twitter in that message!")
+            link = urlparse(message.content)
+            result = link._replace(netloc='vxtwitter.com').geturl()
+            print(result)
+            await message.reply(content = result)
 
 intents = discord.Intents.default()
 intents.message_content = True
