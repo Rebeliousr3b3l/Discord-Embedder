dbdbb2735bbd04aa18ea635fcd14ec839a7ae0ed
rebelliousr3b3l <jdfortner00@gmail.com>
2023-02-12 00:01:38 -0600

    Added necessary components for deleting embeds.

--- bot.py
+++ bot.py
@@ -1,15 +1,32 @@
-import os
-import discord
-import re
-from urllib.parse import urlparse
-from dotenv import load_dotenv
+#!/bin/env python3
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-# https://stackoverflow.com/questions/839994/extracting-a-url-in-python
+import discord
+import asyncio
+import sqlite3
+import logging
+import os
+import re
+from datetime import datetime
+from urllib.parse import urlparse
+
+# Setup a stream handler to write data to stdout.
+# Stdout is piped directly to the web console
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+default_handler = logging.StreamHandler()
+default_handler.setFormatter(formatter)
+
+# install the handler
+logger.addHandler(default_handler)
+
 regex=r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
 
+
 class MyClient(discord.Client):
+    def __init__(self, con):
+        """Called upon instanciation"""
+        self.con = con
+        super().__init__()
+        
     async def on_ready(self):
         print(f'Logged on as {self.user}')
     
@@ -28,8 +45,10 @@ class MyClient(discord.Client):
         
         # only send messages if there were matches
         if len(replacements) > 0:
-            # join the urls together by newlines 
+            # join the urls together by newlines
             await message.reply(content = "\n".join(replacements))
+            await asyncio.sleep(2)
+            await message.edit(suppress=True)
     
     # if url contains a twitter link, replace the hostname
     # with vxtwitter. Returns False if not a twitter link.
@@ -41,8 +60,10 @@ class MyClient(discord.Client):
             return False
 
 
-intents = discord.Intents.default()
-intents.message_content = True
+# Instanciate a connection to an sqlite database
+# this will be used in our client
+con = sqlite3.connect(os.environ["DATABASE_URL"])
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+# Instanciate a client object and start it. 
+client = MyClient(con) 
+asyncio.create_task(client.start(os.environ['DISCORD_TOKEN']))
