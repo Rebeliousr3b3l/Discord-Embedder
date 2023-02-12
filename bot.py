399941800e4927c9338ab262e1705b02dd3270f4
rebelliousr3b3l <jdfortner00@gmail.com>
2023-02-12 00:05:00 -0600

    Added embed deletion and modfiied discord client launching.

--- bot.py
+++ bot.py
@@ -2,21 +2,10 @@
 
 import discord
 import asyncio
-import sqlite3
-import logging
 import os
 import re
 from datetime import datetime
 from urllib.parse import urlparse
-
-# Setup a stream handler to write data to stdout.
-# Stdout is piped directly to the web console
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-default_handler = logging.StreamHandler()
-default_handler.setFormatter(formatter)
-
-# install the handler
-logger.addHandler(default_handler)
 
 regex=r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
 
@@ -58,11 +47,6 @@ class MyClient(discord.Client):
             return link._replace(netloc='vxtwitter.com').geturl()
         else:
             return False
-
-
-# Instanciate a connection to an sqlite database
-# this will be used in our client
-con = sqlite3.connect(os.environ["DATABASE_URL"])
 
 # Instanciate a client object and start it. 
 client = MyClient(con) 
