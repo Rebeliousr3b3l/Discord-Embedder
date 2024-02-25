3e23415793be772bfe7723560ca1c1f0f9d42c8d
Rebeliousr3b3l <jdfortner00@gmail.com>
2024-02-25 13:59:13 -0600

    Added support for instagram and tiktok. Fixed spoiler and unembed detection.

--- bot.py
+++ bot.py
@@ -6,25 +6,35 @@ import os
 import re
 from datetime import datetime
 from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 # https://stackoverflow.com/a/840110
 regex=r"(?P<url>https?://[^\s]+)"
 
 
 class MyClient(discord.Client):
-    def __init__(self, con):
-        """Called upon instanciation"""
-        self.con = con
-        super().__init__()
+    #def __init__(self, con):
+    #    """Called upon instanciation"""
+    #    self.con = con
+    #    super().__init__()
         
     async def on_ready(self):
         print(f'Logged on as {self.user}')
     
     async def on_message(self, message):
+        #don't send message if it's from the bot
         if self.user.id == message.author.id:
             return
         
-        if "||" or ("<" and ">") in message.content:
+        #Checking if the link is unembedded
+        if (re.search(r"<.*>", message.content) != None):
+            return
+        
+        #Checking if the link is hidden.
+        if (re.search(r"\|\|.*\|\|", message.content) != None):
             return
 
         # find all urls in the message content, store them in
@@ -40,18 +50,30 @@ class MyClient(discord.Client):
         if len(replacements) > 0:
             # join the urls together by newlines
             await message.reply(content = "\n".join(replacements))
-            await asyncio.sleep(2)
+            await asyncio.sleep(4)
             await message.edit(suppress=True)
     
     # if url contains a twitter link, replace the hostname
     # with vxtwitter. Returns False if not a twitter link.
     def handle_url(self, url):
         link = urlparse(url)
-        if link.hostname == "twitter.com" and link.query == "s=20":
+        if (link.hostname == "twitter.com" or link.hostname == "x.com") and link.query == "s=20":
             return link._replace(netloc='vxtwitter.com').geturl()
+        
+        elif (link.hostname == "tiktok.com" or link.hostname == "www.tiktok.com"):
+            return link._replace(netloc='vxtiktok.com').geturl()
+        
+        elif (link.hostname == "instagram.com" or link.hostname == "www.instagram.com"):
+            return link._replace(netloc='ddinstagram.com').geturl()
+        
         else:
             return False
 
-# Instanciate a client object and start it. 
-client = MyClient(con) 
-asyncio.create_task(client.start(os.environ['DISCORD_TOKEN']))
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(TOKEN)
+
+
+
